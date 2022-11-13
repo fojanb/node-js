1156b1f8a1ef311ad7a041564a1de9615ad984a2
fojanb <fozhanmail@gmail.com>
2022-11-13 15:18:07 -0800

    Built in node modules

---
+++ node-js-John Smilga/3-modules.js
@@ -0,0 +1,31 @@
+/*  
+______________________________
+app.js is the ROOT module :
+    app.js     
+  /...|...\
+YY...ZZ...XX
+/|\ /|\  /|\
+...      ...
+______________________________
+CommonJS, every file in node is module (by default)
+module -> Encapsulated code (share minimum)
+___<SOME IMPORTANT MODULES (node and npm)>___
+Http (built-in node.js module) -> For creating a server
+fs (built-in node.js module)   -> For CRUD files
+URL (built-in node.js module)  -> For working with paths
+events (built-innode.js module) -> For raising an event
+uc (NPM package)
+Formidable (NPM package) -> For uploading a file
+Nodemailer (NPM package) -> For sending an e-mail
+mysql (NPM package)
+<FOR MORE INFO HEAD OVER TO nodejs.org>
+______________________________
+*/
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+const myData = require("./6-alternative-syntax");
+require("./7-mind-grenade");
+// sayHi(names.name.fojan);
+// sayHi(names.name.makan);
+// costume module ---> require("./jsFileName") relative path
+// built-in module ---> require("moduleName")
--- node-js-John Smilga/app.js
+++ node-js-John Smilga/app.js
@@ -1,29 +1,6 @@
-/*  
-______________________________
-app.js is the ROOT module :
-    app.js     
-  /...|...\
-YY...ZZ...XX
-/|\ /|\  /|\
-...      ...
-______________________________
-CommonJS, every file in node is module (by default)
-module -> Encapsulated code (share minimum)
-___<SOME IMPORTANT MODULES>___
-Http (node.js module) -> For creating a server
-fs (node.js module)   -> For CRUD files
-URL (node.js module)  -> For working with paths
-events (node.js module) -> For raising an event
-uc (NPM package)
-Formidable (NPM package) -> For uploading a file
-Nodemailer (NPM package) -> For sending an e-mail
-mysql (NPM package)
-<FOR MORE INFO HEAD OVER TO nodejs.org>
-______________________________
-*/
-const names = require("./4-names");
-const sayHi = require("./5-utils");
-const myData = require("./3-alternative-syntax");
-require("./6-mind-grenade");
-// sayHi(names.name.fojan);
-// sayHi(names.name.makan);
+const os = require("os");
+// Info about the current user ->
+const user = os.userInfo();
+// Let's returns the system uptime in number of seconds ->
+const upTime = os.uptime();
+console.log(`The system uptime in number of seconds : ${os.uptime()}`);
