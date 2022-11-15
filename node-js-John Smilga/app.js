ebd45ea7206cad0fe6d0665d0a559d5bd3644348
Fojan Babaali <fojan.Babaali@carmanah.com>
2022-11-14 16:50:00 -0800

    Update

--- node-js-John Smilga/app.js
+++ node-js-John Smilga/app.js
@@ -38,4 +38,6 @@ in the working directory of your project.
 const _ = require("lodash");
 const items = [1, [2, [3, [4]]]];
 const newItems = _.flattenDeep(items);
-console.log(newItems);
+console.log("Hello Makan");
+
+
