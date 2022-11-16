5e2bd66d0b350af460ac8991f26d0c8d635a7238
fojanb <fozhanmail@gmail.com>
2022-11-15 20:18:51 -0800

    Lodash and Nodemon added

--- node-js-John Smilga/app.js
+++ node-js-John Smilga/app.js
@@ -2,19 +2,21 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   // User Endpoint 👉 req.url
   // Common practice for homepage is "/"
-  if (req.url == "/") {
-    res.end("Welcome to our homepage (http://localhost:3000/)");
+  if (req.url === "/") {
+    res.end("Welcome to our HOMEPAGE (http://localhost:5050/)");
+    return;
   }
-  if (req.url == "/about") {
-    res.end("Welcome to our about page (http://localhost:3000/about)");
+  if (req.url === "/about") {
+    res.end("Welcome to our ABOUT PAGE (http://localhost:5050/about)");
+    return;
   }
   // Default response as below 👇
   res.end(`<h1>Ooops!</h1>
-            <p>http://localhost:3000${req.url} does not exist in our 
+            <p>http://localhost:5050${req.url} does not exist in our 
             web app</p>
-            <a href="/">Back to homepage</a>`);
+            <a href="/">Back to HOMEPAGE</a>`);
 });
-server.listen(3000, () =>
-  console.log("Server listening on http://localhost:3000/ ...")
-); //The server object listens on port 3000
+server.listen(5050, () =>
+  console.log("Server listening on http://localhost:5050/ ...")
+); //The server object listens on port 5050
 
