e56f444af13ca5f0dbef7f55d4861533719c49d5
fojanb <fozhanmail@gmail.com>
2022-11-17 21:18:39 -0800

    read file

--- node-js-John Smilga/14-lodashANDnodemon-package.js
+++ node-js-John Smilga/14-lodashANDnodemon-package.js
@@ -39,3 +39,4 @@ const _ = require("lodash");
 const items = [1, [2, [3, [4]]]];
 const newItems = _.flattenDeep(items);
 console.log("Hello Amir");
+console.log(newItems);
--- node-js-John Smilga/app.js
+++ node-js-John Smilga/app.js
@@ -1,22 +1,37 @@
 const http = require("http");
+const { readFile, writeFile } = require("fs");
+
 const server = http.createServer((req, res) => {
   // User Endpoint 👉 req.url
   // Common practice for homepage is "/"
   if (req.url === "/") {
-    res.end("Welcome to our HOMEPAGE (http://localhost:5050/)");
+    // readFile() is asynchronous so then event-loop will offload it.
+    readFile("./content/Home/index.html", "utf8", (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.end(result);
+      }
+    });
+
     return;
   }
   if (req.url === "/about") {
-    res.end("Welcome to our ABOUT PAGE (http://localhost:5050/about)");
+    readFile("./content/About/about.html", "utf8", (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.end(result);
+      }
+    });
     return;
   }
   // Default response as below 👇
   res.end(`<h1>Ooops!</h1>
-            <p>http://localhost:5050${req.url} does not exist in our 
+            <p>http://localhost:8080${req.url} does not exist in our 
             web app</p>
-            <a href="/">Back to HOMEPAGE</a>`);
+            <a href="/">Back to homepage</a>`);
 });
-server.listen(5050, () =>
-  console.log("Server listening on http://localhost:5050/ ...")
-); //The server object listens on port 5050
-
+server.listen(8080, () =>
+  console.log("Server listening on http://localhost:8080/ ...")
+); //The server object listens on port 8080
