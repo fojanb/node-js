0031efc5804f8d8b77ef3e82b241e9d4c43bb3a4
fojanb <fozhanmail@gmail.com>
2022-11-21 22:49:39 -0800

    events in node

--- node-js-John Smilga/app.js
+++ node-js-John Smilga/app.js
@@ -1,36 +1,8 @@
-const { readFile, writeFile } = require("fs").promises;
-// const util = require("util");
-// const readFilePromise = util.promisify(readFile);
-// const writeFilePromise = util.promisify(writeFile);
-
-
-// showText = Wrapper function for our Promise
-const showText = async () => {
-  try {
-    const first = await readFile("./content/Home/index.html", "utf8");
-    const second = await readFile("./content/About/about.html", "utf8");
-    await writeFile(
-      "./content/mind-grenade.txt",
-      `${first} ----------------- ${second}`
-    );
-    console.log(first, second);
-  } catch (err) {
-    console.log(err);
-  }
-};
-showText();
-
-// const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     readFile(path, "utf8", (err, data) => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         resolve(data);
-//       }
-//     });
-//   });
-// };
-// getText("./content/Home/index.html")
-//   .then((data) => console.log(data))
-//   .catch((err) => console.log(err));
+const EventEmitter = require("events");
+const cutomeEmitter = new EventEmitter();
+// on ---> listen for an event
+// emit--> emit an event
+cutomeEmitter.on("response", () => {
+  console.log("data recieved");
+});
+cutomeEmitter.emit("response");
