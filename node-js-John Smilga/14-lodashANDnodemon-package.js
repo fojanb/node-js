423a61a0bb2d4cba0c35a48680051a10ee9740fd
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-06-21 14:26:01 -0700

    Add some notes about node

--- node-js-John Smilga/14-lodashANDnodemon-package.js
+++ node-js-John Smilga/14-lodashANDnodemon-package.js
@@ -33,7 +33,7 @@ ___________________________
 📌package.json 👉 Manifest file (stores important info about project/package)
 -manual approach (create package.json in the *root directory*, create properties etc)
 -npm init (step by step, press enter to skip)
--npm init -y (everything default)
+-npm init -y (everything default, and then you can get back to it and change the settings like author)
 📌npm init:
 This utility will walk you through creating a package.json file.
 It only covers the most common items, and tries to guess sensible defaults.
@@ -44,13 +44,29 @@ and exactly what they do.
 Use `npm install <pkg>` afterwards to install a package and
 save it as a dependency in the package.json file.
 
-👉FACT : If you want to run (i.e. npm start in react app) a forked npm repository from github
+👉FACT : If you want to run (i.e. npm start in react app) a forked npm project repository from github
 you must type npm install in the terminal to install all of the dependencies that comes with that
 project and already git ignored. other than that you will run into a nasty error.
+
+👉FACT : Note that React App will create package.json for you automatically, 
+no need to 'npm  init'.
+
+👉FACT : After creating package.json file via 'npm init' in the root directory of you project,
+you can go ahead and install any npm package that you like, for example :
+'npm i lodash' and after finishing the installation, you will notice that a new folder named 'node_modules'
+has been created in the root directory. WHY? because the author of that npm package had used some other
+npm packages (aka dependencies) in order to create his own npm package. so by doing 'npm install lodash'
+you basically install all the rest of npm dependencies that comes with that lodash package.
+
+👉FACT : Note that we never push 'node_modules' folder onto github. Always 'git ignore' that folder.
+If you want to run (i.e. npm start in react app) a forked npm project repository from github
+you must type npm install in the terminal to install all of the dependencies that comes with that
+project and already git ignored by the author of that repository. 
+Other than that you will run into a nasty error.
 ___________________________
 
-👉FACT : Nodemon is a popular tool that is used for the development o
-f applications based on node. js. It simply restarts the node 
+👉FACT : Nodemon is a popular tool that is used for the development 
+of applications based on node. js. It simply restarts the node 
 application whenever it observes the changes in the file present 
 in the working directory of your project.
 */
