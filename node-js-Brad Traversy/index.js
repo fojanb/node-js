39df706745fffbd8f4b13b57da975f37ffbc433a
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-06-27 14:58:59 -0700

    Add comments

--- node-js-Brad Traversy/index.js
+++ node-js-Brad Traversy/index.js
@@ -1,2 +1,8 @@
+// ES6's fetures -> Destructuring
 const { personMaker } = require("./person.js");
 personMaker({ name: "Makan", age: 4 });
+
+// OR
+
+// const personMaker = require("./person.js");
+// personMaker.personMaker({ name: "Makan", age: 4 });
