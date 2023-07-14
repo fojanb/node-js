8a0dc318264f0b427ae266b714fb14f09b92212c
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-07-14 15:20:02 -0700

    Add notes

--- node-js-John Smilga/3-task-manager/controllers/tasks.js
+++ node-js-John Smilga/3-task-manager/controllers/tasks.js
@@ -27,3 +27,22 @@ const deleteTask = (req, res) => {
   res.send(`Delete one task with id : ${id}`);
 };
 module.exports = { getTask, getTasks, postTask, patchTask, deleteTask };
+
+/*
+👉So far you must have good grasp of why we need Postman or Insomnia tools:
+Because GET request is the only default http request on browser, for testing other http method 
+we have to code a working application OR using Postman/Insomnia
+You can not test a for example POST request URL inside the url field.
+
+👉Also you must already know that why we use a database like MongoDB instead of
+storing our data inside a simple array.
+*/
+
+/*
+🔮REST API Architecture:
+GET        /api/tasks     👉 Get all tasks
+POST       /api/tasks     👉 Create task
+GET        /api/tasks/:id 👉 Get one task
+PUT/PATCH  /api/tasks/:id 👉 Update task
+DELETE     /api/tasks/:id 👉 Delete task
+*/
