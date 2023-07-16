7ffadcb218f456d7b458dc82a524d09730bd0e66
fojanb <fozhanmail@gmail.com>
2023-07-15 19:10:42 -0700

    Add the POST method logic to the controller

---
+++ node-js-John Smilga/3-task-manager/db/connect.js
@@ -0,0 +1,8 @@
+const connection = require("mongoose");
+const connectionString =
+  "mongodb+srv://Makan:<password>@task-manager.s6bxdu1.mongodb.net/";
+
+
+
+// Collections in monogoDB are tables in regular database
+// Documents are simple key-value pairs
