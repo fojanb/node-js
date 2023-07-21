2a3241dcc59540526f26ec0e5ee237cf9e9abf76
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-07-21 10:54:11 -0700

    Add comments

--- node-js-John Smilga/3-task-manager/taskList.js
+++ node-js-John Smilga/3-task-manager/taskList.js
@@ -1,3 +1,4 @@
-// DATABASE
+// Basic array to store our data (Basic array as a DATABASE) which is not really
+// a practical approach. So lets setup a real databse (mongoDB)
 let taskList = [];
 module.exports = taskList;
